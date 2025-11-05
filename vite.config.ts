93454baab8315df2729f17dbd691e07a4b299a05
Quasardilla <casualquasar@gmail.com>
2025-11-05 11:39:28 -0500

    setting up frontend and backend

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import svgrPlugin from "vite-plugin-svgr";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  root: "./frontend",
+  server: {
+    port: 5106,
+    proxy: {
+      "/api": "Enter your backend host with port inside the quotes",
+    },
+  },
+  plugins: [
+    react(),
+    svgrPlugin({
+      svgrOptions: {
+        icon: true,
+      },
+    }),
+  ],
+  build: {
+    outDir: "./build",
+  }
+});
